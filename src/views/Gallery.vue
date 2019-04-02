905accad7ff6a07fb72b0d41d9c912537e732abd
Nicolas Swijngedouw <nicolas.swijngedouw@capgemini.com>
2019-04-03 00:39:59 +0200

    minor changes to layout and logic

--- src/views/Gallery.vue
+++ src/views/Gallery.vue
@@ -2,15 +2,26 @@
     <v-container>
         <v-layout>
             <v-flex xs12>
-                <v-carousel height="800" class="mt-2">
-                    <v-carousel-item
-                        v-for="(pic, index) in pictures"
+                <v-carousel height="800" class="mt-2" :cycle="false">
+                    <v-carousel-item v-for="(pic, index) in pictures"
                         :src="pic.src"
-                        :key="index"
-                    >
-                    <v-btn flat @click="showInfo(index)">Info</v-btn>
+                        :key="index">
+                    <v-btn flat @click.native.stop="dialog = !dialog" v-on:click="openDialog(index)"><v-icon x-large>info</v-icon></v-btn>
                     <div class="title"> {{ pic.title }} </div>
+                    
                     </v-carousel-item>
+                    <v-dialog dark v-model="dialog">
+                        <v-card v-model="info">
+                            <v-img :src="info.src" aspect-ratio="2.75">
+                            </v-img>
+                            <v-card-title>
+                                {{ info.title }}                                
+                            </v-card-title>
+                            <v-card-text>
+                                {{ info.src}}
+                            </v-card-text>
+                        </v-card>
+                    </v-dialog>
                 </v-carousel>
             </v-flex>
         </v-layout>
@@ -25,12 +36,18 @@ export default {
                 {src: 'https://firebasestorage.googleapis.com/v0/b/oneframe-ce302.appspot.com/o/stas%2Fstas1.jpg?alt=media&token=b4613366-0aba-407e-a10c-d5b9c07e302f', id:1, title:'Stas is kostuum'},
                 {src: 'https://firebasestorage.googleapis.com/v0/b/oneframe-ce302.appspot.com/o/stas%2Fstas2.jpg?alt=media&token=f5ec874c-4db2-45f5-8b43-fa1b3e4adfdd', id:2, title:'Stas op TD'},
                 {src: 'https://firebasestorage.googleapis.com/v0/b/oneframe-ce302.appspot.com/o/stas%2Fstas3.jpg?alt=media&token=364e6708-26a2-4659-84ca-61811556d91b', id:3, title:'Stas viert Halloween'}
-            ]
+            ],
+            dialog: false,
+            info: []
+            
         }
     },
     methods: {
         showInfo(picId){
             alert(this.pictures[picId].title);
+        },
+        openDialog(index){
+            this.info = this.pictures[index]
         }
     }
 }
--- src/views/Navigation/NavigationDrawer.vue
+++ src/views/Navigation/NavigationDrawer.vue
@@ -1,13 +1,13 @@
 <template>
     <nav>
-        <v-toolbar flat app>
+        <v-toolbar flat app class="blue lighten-1">
             <v-toolbar-side-icon class="hidden-sm-and-up" @click.native.stop="drawer = !drawer"></v-toolbar-side-icon>
              <v-toolbar-title>
                     <router-link to="/" tag="span" style="cursor : pointer">OneFRAME</router-link>
             </v-toolbar-title> 
             <v-spacer></v-spacer>
             <v-toolbar-items class="hidden-xs-only">
-                 <v-btn flat v-for="item in menuItems" :key="item.title" router :to="item.link">
+                 <v-btn dark flat v-for="item in menuItems" :key="item.title" router :to="item.link">
                      <v-icon left dark>{{item.icon}}</v-icon>
                      {{ item.title }}
 
@@ -21,7 +21,7 @@
             </v-toolbar-items>
             
         </v-toolbar>
-        <v-navigation-drawer temporary="" v-model="drawer" app class="grey">
+        <v-navigation-drawer temporary="" v-model="drawer" app class="blue lighten-1">
             <v-list>
                 <v-list-tile v-for="item in menuItems" :key="item.title" router :to="item.link">
                     <v-list-tile-action>
@@ -29,6 +29,12 @@
                     </v-list-tile-action>                    
                     <v-list-tile-content>{{item.title}}</v-list-tile-content>
                 </v-list-tile>
+                <v-list-tile v-if="userIsAuthenticated"
+                    flat
+                    @click="onLogout">
+                    <v-list-tile-action><v-icon>exit_to_app</v-icon></v-list-tile-action>                    
+                    <v-list-tile-content>Logout</v-list-tile-content>                    
+                 </v-list-tile>
             </v-list>
         </v-navigation-drawer>
     </nav>
